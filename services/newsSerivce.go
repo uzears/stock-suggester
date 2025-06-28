c41557679337567f5c4ec1d38c093562b79dd801
uzear shaikh <shaikhuzear@gmail.com>
2025-06-28 13:48:39 +0530

    changed depreceted function to io

--- services/newsSerivce.go
+++ services/newsSerivce.go
@@ -3,7 +3,7 @@ package services
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 
 	"github.com/uzears/stock-suggester/config"
@@ -19,12 +19,12 @@ func FetchNews() ([]models.NewsArticle, error) {
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 	var parsed map[string]interface{}
 	json.Unmarshal(body, &parsed)
 
 	var articles []models.NewsArticle
-	for _, item := range parsed["articles"].([]interface{}) {
+	for _, item := range parsed["artiscles"].([]interface{}) {
 		data := item.(map[string]interface{})
 		articles = append(articles, models.NewsArticle{
 			Title: data["title"].(string),
